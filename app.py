12f325d9264d21f61abeddc2dd86860937a347f8
cyberboysumanjay <cyberboysumanjay@gmail.com>
2022-09-16 19:26:48 +0530

    Update logic to fetch songid

--- app.py
+++ app.py
@@ -6,13 +6,14 @@ from traceback import print_exc
 from flask_cors import CORS
 
 app = Flask(__name__)
-app.secret_key = os.environ.get("SECRET",'thankyoutonystark#weloveyou3000')
+app.secret_key = os.environ.get("SECRET", 'thankyoutonystark#weloveyou3000')
 CORS(app)
 
 
 @app.route('/')
 def home():
     return redirect("https://cyberboysumanjay.github.io/JioSaavnAPI/")
+
 
 @app.route('/song/')
 def search():
@@ -21,28 +22,29 @@ def search():
     query = request.args.get('query')
     lyrics_ = request.args.get('lyrics')
     songdata_ = request.args.get('songdata')
-    if lyrics_ and lyrics_.lower()!='false':
+    if lyrics_ and lyrics_.lower() != 'false':
         lyrics = True
-    if songdata_ and songdata_.lower()!='true':
+    if songdata_ and songdata_.lower() != 'true':
         songdata = False
     if query:
         return jsonify(jiosaavn.search_for_song(query, lyrics, songdata))
     else:
         error = {
             "status": False,
-            "error":'Query is required to search songs!'
+            "error": 'Query is required to search songs!'
         }
         return jsonify(error)
+
 
 @app.route('/song/get/')
 def get_song():
     lyrics = False
     id = request.args.get('id')
     lyrics_ = request.args.get('lyrics')
-    if lyrics_ and lyrics_.lower()!='false':
+    if lyrics_ and lyrics_.lower() != 'false':
         lyrics = True
     if id:
-        resp = jiosaavn.get_song(id,lyrics)
+        resp = jiosaavn.get_song(id, lyrics)
         if not resp:
             error = {
                 "status": False,
@@ -58,41 +60,44 @@ def get_song():
         }
         return jsonify(error)
 
+
 @app.route('/playlist/')
 def playlist():
     lyrics = False
     query = request.args.get('query')
     lyrics_ = request.args.get('lyrics')
-    if lyrics_ and lyrics_.lower()!='false':
+    if lyrics_ and lyrics_.lower() != 'false':
         lyrics = True
     if query:
         id = jiosaavn.get_playlist_id(query)
-        songs = jiosaavn.get_playlist(id,lyrics)
+        songs = jiosaavn.get_playlist(id, lyrics)
         return jsonify(songs)
     else:
         error = {
             "status": False,
-            "error":'Query is required to search playlists!'
+            "error": 'Query is required to search playlists!'
         }
         return jsonify(error)
+
 
 @app.route('/album/')
 def album():
     lyrics = False
     query = request.args.get('query')
     lyrics_ = request.args.get('lyrics')
-    if lyrics_ and lyrics_.lower()!='false':
+    if lyrics_ and lyrics_.lower() != 'false':
         lyrics = True
     if query:
         id = jiosaavn.get_album_id(query)
-        songs = jiosaavn.get_album(id,lyrics)
+        songs = jiosaavn.get_album(id, lyrics)
         return jsonify(songs)
     else:
         error = {
             "status": False,
-            "error":'Query is required to search albums!'
+            "error": 'Query is required to search albums!'
         }
         return jsonify(error)
+
 
 @app.route('/lyrics/')
 def lyrics():
@@ -111,15 +116,15 @@ def lyrics():
             return jsonify(response)
         except Exception as e:
             error = {
-            "status": False,
-            "error": str(e)
+                "status": False,
+                "error": str(e)
             }
             return jsonify(error)
-        
+
     else:
         error = {
             "status": False,
-            "error":'Query containing song link or id is required to fetch lyrics!'
+            "error": 'Query containing song link or id is required to fetch lyrics!'
         }
         return jsonify(error)
 
@@ -129,35 +134,35 @@ def result():
     lyrics = False
     query = request.args.get('query')
     lyrics_ = request.args.get('lyrics')
-    if lyrics_ and lyrics_.lower()!='false':
+    if lyrics_ and lyrics_.lower() != 'false':
         lyrics = True
 
     if 'saavn' not in query:
-        return jsonify(jiosaavn.search_for_song(query,lyrics,True))
+        return jsonify(jiosaavn.search_for_song(query, lyrics, True))
     try:
         if '/song/' in query:
             print("Song")
             song_id = jiosaavn.get_song_id(query)
-            song = jiosaavn.get_song(song_id,lyrics)
+            song = jiosaavn.get_song(song_id, lyrics)
             return jsonify(song)
 
         elif '/album/' in query:
             print("Album")
             id = jiosaavn.get_album_id(query)
-            songs = jiosaavn.get_album(id,lyrics)
+            songs = jiosaavn.get_album(id, lyrics)
             return jsonify(songs)
 
         elif '/playlist/' or '/featured/' in query:
             print("Playlist")
             id = jiosaavn.get_playlist_id(query)
-            songs = jiosaavn.get_playlist(id,lyrics)
+            songs = jiosaavn.get_playlist(id, lyrics)
             return jsonify(songs)
 
     except Exception as e:
         print_exc()
         error = {
             "status": True,
-            "error":str(e)
+            "error": str(e)
         }
         return jsonify(error)
     return None
@@ -165,4 +170,4 @@ def result():
 
 if __name__ == '__main__':
     app.debug = True
-    app.run(host='0.0.0.0', port=5000, use_reloader=True, threaded=True)
+    app.run(host='0.0.0.0', port=5100, use_reloader=True, threaded=True)
--- jiosaavn.py
+++ jiosaavn.py
@@ -4,13 +4,15 @@ import helper
 import json
 from traceback import print_exc
 
-def search_for_song(query,lyrics,songdata):
+
+def search_for_song(query, lyrics, songdata):
     if query.startswith('http') and 'saavn.com' in query:
         id = get_song_id(query)
         return get_song(id, lyrics)
 
     search_base_url = endpoints.search_base_url+query
-    response = requests.get(search_base_url).text.encode().decode('unicode-escape')
+    response = requests.get(
+        search_base_url).text.encode().decode('unicode-escape')
     response = json.loads(response)
     song_response = response['songs']['data']
     if not songdata:
@@ -23,35 +25,40 @@ def search_for_song(query,lyrics,songdata):
             songs.append(song_data)
     return songs
 
-def get_song(id,lyrics):
+
+def get_song(id, lyrics):
     try:
         song_details_base_url = endpoints.song_details_base_url+id
-        song_response = requests.get(song_details_base_url).text.encode().decode('unicode-escape')
+        song_response = requests.get(
+            song_details_base_url).text.encode().decode('unicode-escape')
         song_response = json.loads(song_response)
-        song_data = helper.format_song(song_response[id],lyrics)
+        song_data = helper.format_song(song_response[id], lyrics)
         if song_data:
             return song_data
     except:
         return None
 
+
 def get_song_id(url):
     res = requests.get(url, data=[('bitrate', '320')])
     try:
-        return res.text.split('"song":{"type":"')[1].split('","image":')[0].split('"id":"')[-1]
-    except IndexError:
         return(res.text.split('"pid":"'))[1].split('","')[0]
+    except IndexError:
+        return res.text.split('"song":{"type":"')[1].split('","image":')[0].split('"id":"')[-1]
 
-def get_album(album_id,lyrics):
+
+def get_album(album_id, lyrics):
     songs_json = []
     try:
         response = requests.get(endpoints.album_details_base_url+album_id)
         if response.status_code == 200:
             songs_json = response.text.encode().decode('unicode-escape')
             songs_json = json.loads(songs_json)
-            return helper.format_album(songs_json,lyrics)
+            return helper.format_album(songs_json, lyrics)
     except Exception as e:
         print(e)
         return None
+
 
 def get_album_id(input_url):
     res = requests.get(input_url)
@@ -60,17 +67,19 @@ def get_album_id(input_url):
     except IndexError:
         return res.text.split('"page_id","')[1].split('","')[0]
 
-def get_playlist(listId,lyrics):
+
+def get_playlist(listId, lyrics):
     try:
         response = requests.get(endpoints.playlist_details_base_url+listId)
         if response.status_code == 200:
             songs_json = response.text.encode().decode('unicode-escape')
             songs_json = json.loads(songs_json)
-            return helper.format_playlist(songs_json,lyrics)
+            return helper.format_playlist(songs_json, lyrics)
         return None
     except Exception:
         print_exc()
         return None
+
 
 def get_playlist_id(input_url):
     res = requests.get(input_url).text
@@ -78,6 +87,7 @@ def get_playlist_id(input_url):
         return res.split('"type":"playlist","id":"')[1].split('"')[0]
     except IndexError:
         return res.split('"page_id","')[1].split('","')[0]
+
 
 def get_lyrics(id):
     url = endpoints.lyrics_base_url+id
